f87b6b6f626898c9d1a6e5ec82c36514ff708de7
FatemaRashed <Fatema_rashed@outlook.com>
2023-08-02 19:53:33 +0300

    Setup Teachers List(Not Finish yet  )

---
+++ Eshara/Eshara/Teachers.swift
@@ -0,0 +1,22 @@
+//
+//  Teachers.swift
+//  Eshara
+//
+//  Created by iOSdev on 02/08/2023.
+//
+
+import Foundation
+import UIKit
+
+
+struct Teacher
+{
+    var name: String
+    var rateView: Double
+    var location: String
+    var price: Double
+    var image: UIImage?
+    var color: UIColor
+    
+    
+}
---
+++ Eshara/Eshara/TeachersData.swift
@@ -0,0 +1,17 @@
+//
+//  TeachersData.swift
+//  Eshara
+//
+//  Created by iOSdev on 02/08/2023.
+//
+
+import Foundation
+
+
+struct Teacherdata
+{
+    var teacher: [Teacher] 
+    var teacherData: [String]
+    
+   
+}
