5e1a479c83f84d558ee9032fbd51bbcdc636ffaa
ayat <aalkhawaja77@gmail.com>
2023-08-24 16:11:42 +0300

    zaki push thursday

--- Eshara/Eshara/Zaki/MyProfileViewController.swift
+++ Eshara/Eshara/Zaki/MyProfileViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
-class MyProfileViewController: UIViewController {
+class MyProfileViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     
     @IBAction func addFriend(_ sender: UIButton) {
@@ -17,12 +19,52 @@ class MyProfileViewController: UIViewController {
       
     @IBOutlet weak var myProfile: UICollectionView!
     
+    var reviews = [Review]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        getReviews()
     }
     
+    func getReviews() {
+        let ref = Database.database().reference()
+        let uid = Auth.auth().currentUser?.uid
+        
+//        ref.child("user").child(uid!).child("Review").observeSingleEvent(of: .value, with: {
+        ref.child("user").child(uid!).child("Review").observeSingleEvent(of: .value, with: {
+            (snapshot) in guard let result = snapshot.children.allObjects as? [DataSnapshot] else {return}
+            
+            for review in result {
+                let value = review.value as? NSDictionary
+                
+                let name = value!["Name"] as! String
+                
+                self.reviews.append(Review(name: name, rate: "", comment: ""))
+            }
+            
+            self.myProfile.reloadData()
+        })
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return reviews.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "commentCell", for: indexPath) as! MyProfileCollectionViewCell
+        
+        let comment = reviews[indexPath.row]
+        
+        cell.playerName.text = comment.name
+        
+        return cell
+    }
 
     
 
---
+++ Eshara/Eshara/Zaki/User+Reviews.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  Eshara
+//
+//  Created by iOSdev on 23/08/2023.
+//
+
+import Foundation
+
+struct Review {
+    var name: String
+    var rate: String
+    var comment: String
+}
+
+struct User {
+    var name: String
+    var image: String
+    var points: Int
+    var ranking: String
+}
